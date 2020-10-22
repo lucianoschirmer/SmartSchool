68f9eabac08f0e304d2516d38c1c38decac06a79
Luciano Schirmer <lucianoschirmerbrasil@gmail.com>
2020-10-21 22:48:10 -0300

    criado pages e suas rotas

---
+++ src/app/titulo/titulo.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-titulo',
+  templateUrl: './titulo.component.html',
+  styleUrls: ['./titulo.component.css']
+})
+export class TituloComponent implements OnInit {
+
+  public titulo: 'titulo';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
