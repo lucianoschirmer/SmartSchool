5bcc938db9d6ada19261faf8cf7f234e9d7658e9
Luciano Schirmer <lucianoschirmerbrasil@gmail.com>
2020-10-25 20:11:40 -0300

    criado classe aluno e classe professor

---
+++ SeuNersoSchool-WebAPI/Controllers/ProfessorController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SeuNersoSchool_WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProfessorController
+    {
+
+    }
+
+}
